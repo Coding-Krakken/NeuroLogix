07b6156785e567a5f69e7a8295fc74f6a4c81840
Coding-Krakken <davidtraversmailbox@gmail.com>
2026-03-09 14:17:36 -0400

    fix(ci): remove stale tsbuildinfo and deflake sleep test

--- packages/core/src/utils/index.test.ts
+++ packages/core/src/utils/index.test.ts
@@ -33,7 +33,7 @@ describe('Utils', () => {
       const start = Date.now();
       await sleep(100);
       const end = Date.now();
-      expect(end - start).toBeGreaterThanOrEqual(100);
+      expect(end - start).toBeGreaterThanOrEqual(95);
     });
   });
 
